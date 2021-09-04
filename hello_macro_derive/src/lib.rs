118feba0d4241b0b0c4bfc0764f1ac412181d81b
Alexander Nookinov <anookinov@gmail.com>
2021-09-04 20:49:17 +0700

    create hello_macro and hello_macro_derive crates

---
+++ hello_macro_derive/src/lib.rs
@@ -0,0 +1,46 @@
+extern crate proc_macro;
+
+use proc_macro::TokenStream;
+use quote::quote;
+use syn;
+
+#[proc_macro_derive(HelloMacro)]
+pub fn hello_macro_derive(input: TokenStream) -> TokenStream {
+    // Construct a representationo of Rust code as a syntax tree
+    // that we can manipulate
+    let ast = syn::parse(input).unwrap();
+
+    // Build the trait implementation
+    impl_hello_macro(&ast)
+}
+
+// Tokenstream -> DeriveInput
+// DeriveInput {
+//     // --snip--
+
+//     ident: Ident {
+//         ident: "Pancakes",
+//         span: #0 bytes(95..103)
+//     },
+//     data: Struct(
+//         DataStruct {
+//             struct_token: Struct,
+//             fields: Unit,
+//             semi_token: Some(
+//                 Semi
+//             )
+//         }
+//     )
+// }
+
+fn impl_hello_macro(ast: &syn::DeriveInput) -> TokenStream {
+    let name = &ast.ident;
+    let gen = quote! {
+        impl HelloMacro for #name {
+            fn hello_macro() {
+                println!("Hello, Macro! My name is {}!", stringify!(#name));
+            }
+        }
+    };
+    gen.into()
+}
